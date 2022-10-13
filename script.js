709c572a1033a81da1fea4e6b1c06747c2b175ce
Gokul <72689645+Gokulmazz@users.noreply.github.com>
2022-10-13 11:19:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+var divhead=document.getElementById("divhead");
+var a=prompt("enter first number : ");
+var b=prompt("enter second number : ");
+
+divhead.innerText='Greater number is '+Math.max(a,b);
+
+
+
+
